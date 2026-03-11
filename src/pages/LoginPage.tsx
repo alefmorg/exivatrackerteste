a59d0b105405aac4e5465f13db70f49df005d6ab
alefmorg <alefjesus2004@gmail.com>
2026-03-11 15:20:42 -0300

    Change email placeholder text in LoginPage

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -73,7 +73,7 @@ export default function LoginPage() {
               type="email"
               value={email}
               onChange={e => setEmail(e.target.value)}
-              placeholder="operador@guild.com"
+              placeholder="email"
               className="bg-secondary/50 border-border h-9 text-sm"
               required
             />
